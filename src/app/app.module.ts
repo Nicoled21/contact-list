c66b68cb8a99de22e470d9f47b1ab38ee7a59439
Nicole D <52378875+Nicoled21@users.noreply.github.com>
2020-04-09 20:48:29 -0400

    commit to master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactListComponent,
+    ContactCreateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable} from 'rxjs';
+import {Contact} from './contact.model'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private contactsUrl = '/api/contacts'
+  constructor(private http: HttpClient) { }
+
+  public getContacts():Observable<Contact[]> {
+    return this.http.get<Contact[]>(this.contactsUrl);
+  }
+
+  public createContact(contact:Contact) {
+    this.http.post("/api/contact",contact);
+     
+  }
+}
